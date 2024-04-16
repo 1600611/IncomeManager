55586f2612a8816f667247cb6a35e40c5873b147
joelapp2u <joel.angles@app2u.es>
2024-04-16 17:03:43 +0200

    refs #002: creació de nou projecte Swift "Income Manager" i d'alugns directoris de moment provisionals

---
+++ IncomeManager/IncomeManager/IncomeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeManagerApp.swift
+//  IncomeManager
+//
+//  Created by Joel Angles Roca on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct IncomeManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {}
+    }
+}
